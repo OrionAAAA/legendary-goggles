7b438f0a2872b7e382122dc92b0b9256420ed93e
OrionAAAA <38377286+Orionfirst@users.noreply.github.com>
2021-01-22 01:35:18 +0800

    汉诺塔demo

---
+++ src/training6.java
@@ -0,0 +1,25 @@
+/**
+ * Created by Orion on 2020/10/1 16:10
+ */
+
+/**
+ * 汉诺塔
+ */
+public class training6 {
+    public static void main(String[] args) {
+        int nDisks = 3;
+        doTowers(nDisks, 'A', 'B', 'C');
+    }
+
+    public static void doTowers(int topN, char from, char inter, char to) {
+        if (topN == 1){
+            System.out.println("Disk 1 from "
+                    + from + " to " + to);
+        }else {
+            doTowers(topN - 1, from, to, inter);
+            System.out.println("Disk "
+                    + topN + " from " + from + " to " + to);
+            doTowers(topN - 1, inter, from, to);
+        }
+    }
+}
